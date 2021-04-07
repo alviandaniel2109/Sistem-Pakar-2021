f240237d7386e762cc89a8a351f440969e655e7b
alviandaniel2109 <alviandaniel2109@gmail.com>
2021-04-07 21:49:56 +0700

    Tugas

---
+++ Tugas 3 - Naive Bayes/Naive Bayes.py
@@ -0,0 +1,40 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+
+#Load dataset
+wine = datasets.load_breast_cancer()
+
+# print the names of the 13 feature
+print("Features: ", wine.feature_names)
+
+# print the label type of wine(class_0, class_1, class_2)
+print("Labels: ", wine.target_names)
+
+# print data(feature)shape
+wine.data.shape
+
+print (wine.data[0:5])
+
+# print the wine labels (0:Class_0, 1:class_2, 2:class_2)
+print (wine.target)
+
+# Import train_test_split function
+
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3,random_state=109) # 70% training and 30% test
+
+
+#Create a Gaussian Classifier
+gnb = GaussianNB()
+
+#Train the model using the training sets
+gnb.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = gnb.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
